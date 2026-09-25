9ba63bd52e76184aec2b037c0a57ad0883b598a7
Mxpea <a142311@outlook.com>
2026-09-25 18:05:13 +0800

    Add new image asset and custom styles for markdown content
    - Added a new WebP image asset for pull request visualization.
    - Created a custom CSS file to style images within markdown content, ensuring they are responsive and centered.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,24 +7,9 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'QUSTMC',
+			customCss: ['./src/styles/custom.css'],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/QUSTMC/QUST_Wiki' }],
-			sidebar: [
-				{
-					label: '简介',
-					items: [{ autogenerate: { directory: '社团介绍' } }],
-				},
-				{
-					label: '指南',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ autogenerate: { directory: '入服须知' } },
-					],
-				},
-				{
-					label: '参考',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
+			
 		}),
 	],
 });
